bffb261c97ebbff0dfdcc95b1cd03065f5b29eca
SSR <someshwar.sangaraju19@gmail.com>
2025-09-27 15:15:23 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "siva: Hi bhaktha"
-echo "devotee: hloo swamy"
-echo "siva: em viseshalu"
-echo "devotee: meku teliyaniva swamy"
+echo "$1: Hi bhaktha"
+echo "$2: hloo swamy"
+echo "$1: em viseshalu"
+echo "$2: meku teliyaniva swamy"

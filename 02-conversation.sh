e62f3020d18590950c4fe5f3613e5763f5fec002
SSR <someshwar.sangaraju19@gmail.com>
2025-09-28 11:42:22 +0530

    arguments

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "$1: Hi bhaktha"
-echo "$2: hloo swamy"
-echo "$1: em viseshalu"
-echo "$2: meku teliyaniva swamy"
+$person1=siva
+$person2=devotee
+
+
+echo "$person1: Hi bhaktha"
+echo "$person2: hloo swamy"
+echo "$person1: em viseshalu"
+echo "$person2: meku teliyaniva swamy"

f464369d73557bb65c3f2f4a22a4b682c262f0e3
SSR <someshwar.sangaraju19@gmail.com>
2025-09-28 15:45:23 +0530

    Arguments passing through cli

--- 04-variableArguments.sh
+++ 04-variableArguments.sh
@@ -3,7 +3,7 @@
 person1=$1
 person2=$2
 
-echo "person1 :: what are u learning"
-echo "person2 :: I am learning devsecops with aws"
-echo "person1 :: great"
-echo "person2 :: yeah"
+echo "$person1 :: what are u learning"
+echo "$person2 :: I am learning devsecops with aws"
+echo "$person1 :: great"
+echo "$person2 :: yeah"

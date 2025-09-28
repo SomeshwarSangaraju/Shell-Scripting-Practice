594a01d06e275419f15c1c3b4fe65f61709b9622
SSR <someshwar.sangaraju19@gmail.com>
2025-09-28 11:44:44 +0530

    Tight coupling

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-$person1=siva
-$person2=devotee
-
-
-echo "$person1: Hi bhaktha"
-echo "$person2: hloo swamy"
-echo "$person1: em viseshalu"
-echo "$person2: meku teliyaniva swamy"
+echo "siva: Hi bhaktha"
+echo "devotee: hloo swamy"
+echo "siva: em viseshalu"
+echo "devotee: meku teliyaniva swamy"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$person1=siva
+$person2=devotee
+
+
+echo "$person1: Hi bhaktha"
+echo "$person2: hloo swamy"
+echo "$person1: em viseshalu"
+echo "$person2: meku teliyaniva swamy"

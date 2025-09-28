b01b2227ced5f238b66399943da42cdf85133b36
SSR <someshwar.sangaraju19@gmail.com>
2025-09-28 16:17:36 +0530

    specialvariables

--- 07-specialvariables.sh
+++ 07-specialvariables.sh
@@ -7,4 +7,5 @@ echo "sricpt home directory : $HOME"
 echo "User : $USER"
 echo "process ID of the script : $$"
 echo "Current working directory : $PWD"
+sleep 10 &
 echo "PID of last command in the background : $!"

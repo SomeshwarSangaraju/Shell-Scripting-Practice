c18253f99aaf92a71fd458f018d1b74fdb8397f3
SSR <someshwar.sangaraju19@gmail.com>
2025-09-28 17:50:48 +0530

    datatype

--- 08-datatype.sh
+++ 08-datatype.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-siva=("parvathi","vinayaka","subbramanyam")
-
-echo "${siva[@]}"
-
 num1 = 100
 num2 = 200
 
 sum =$(($num1+$num2))
 
 echo "sum : ${sum}"
+
+siva=("parvathi","vinayaka","subbramanyam")
+
+echo "${siva[@]}"
+

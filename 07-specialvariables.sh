c3d52bf94f302ce7832f01d17e449b2f083c1b49
SSR <someshwar.sangaraju19@gmail.com>
2025-09-28 16:12:10 +0530

    special variables

---
+++ 07-specialvariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables passed to the script : $@"
+echo "All variables passed to the script : $*"
+echo "script name : $0"
+echo "sricpt home directory : $home"
+echo "User : $user"
+echo "process ID of the script : $$"
+echo "Current working directory : $pwd"

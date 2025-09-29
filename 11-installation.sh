da1c5b3db0770f24315350069b0996efeb240f40
SSR <someshwar.sangaraju19@gmail.com>
2025-09-29 11:44:52 +0530

    checking root access

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,3 +5,15 @@ User1=$(id -g)
 
 echo "$User"
 echo "$User1"
+
+if [ $User -ne 0 ]; then
+    echo "Please provide root access"
+fi
+
+dnf install mysql -y
+
+if [ $User -ne 0]; then
+    echo "Installing mysql is failure"
+else
+    echo "Installing mysql is success"
+fi

d3b89cefa6b9605c16602e2a99a8d06db2322296
SSR <someshwar.sangaraju19@gmail.com>
2025-09-29 20:22:38 +0530

    loops

--- 19-loops.sh
+++ 19-loops.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$( id -u )
-
-LOG_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOG_FILE/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $LOG_FOLDER
-
-echo "Script started executed at :$(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: please run the script with root previlage"
-    exit 1;
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "ERROR :: Installing $2 is $R failure $N " | tee -a $LOG_FILE
-        exit 1;
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-        echo -e "SUCCESS:: Installing $2 is $G success $N " | tee -a $LOG_FILE
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
+# $@
 
 for package in $@
 do
-    dnf list install $package &>>LOG_FILE
+    # check package is already installed or not
+    dnf list installed $package &>>$LOG_FILE
+
+    # if exit status is 0, already installed. -ne 0 need to install it
     if [ $? -ne 0 ]; then
-        dnf install $package -y
-        VALIDATE $? $package
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
     else
-        echo "SUCCESS :: $package is already exist $Y SKIPPING $N" 
+        echo -e "$package already installed ... $Y SKIPPING $N"
     fi
 done
-

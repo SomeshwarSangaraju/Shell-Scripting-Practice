d02ec5874e19ed8298d77c866234069d909fc606
SSR <someshwar.sangaraju19@gmail.com>
2025-09-30 11:20:38 +0530

    siva

---
+++ 23-recall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "Please run the script with root previlage"
+    exit 11;
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        dnf install $2 -y
+        exit 1
+    else 
+        echo "$2 is already exist"
+    fi
+}
+
+dnf list installed mysql
+VALIDATE $? "mysql"
+
+

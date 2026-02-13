01af94c6c0b2ccc38665d5bdc205fb62bb7f42bf
SSR <someshwar.sangaraju19@gmail.com>
2026-02-13 17:24:57 +0530

    shell scripting

---
+++ 25-while-loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+count=5
+
+echo "Starting countdown..."
+
+while [ $count -gt 0 ]
+do
+  echo "Time left: $count"
+  sleep 1 # Pause for 1 second
+  count=$((count - 1)) # Decrement the count
+done
+
+echo "Times up!"
+
+# while IFS= read -r line
+# do
+#     echo "$line"
+# done < 21-script-1.sh
